66becdca52722faf552ac021ab627544dc8647ed
leo791114 <leo791114@hotmail.com>
2017-07-14 18:37:32 +0800

    Day ten handleCheck event

--- Day eight/main.js
+++ Day eight/main.js
@@ -1,22 +1,30 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 // console.log(ctx);
-ctx.width = window.innerWidth;
-ctx.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 // The strokeStyle sets or returns the colr, gradient, or pattern used for strokes.
 ctx.strokeStyle = '#BADA55';
 // The lineJoin property sets or returns the type of corner created when two lines meet.
 ctx.lineJoin = 'round';
 // The lineCap property sets or returns the style of the end caps of a line. 
 ctx.lineCap = 'round';
+// The lineWidth property sets or returns the current line width
+ctx.lineWidth = 1;
+
+ctx.globalCompositeOperation = 'multiply'
+
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let lineWidthDirection = true;
 
 function draw(e) {
     if (!isDrawing) return;
     // Initializing
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
     ctx.beginPath();
     // Set the start point.
     ctx.moveTo(lastX, lastY);
@@ -25,10 +33,29 @@ function draw(e) {
     ctx.lineTo(e.offsetX, e.offsetY);
     // The stroke() method actually draws the path we have defined with all those moveTo() and lineTo() methods.
     ctx.stroke();
-    console.log(e);
+    // ES6 trick to reassign both lastX and lastY values
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+    // Increase the lineWidth, and after it reaches 100, it starts decreasing.
+    if (ctx.lineWidth > 100 || ctx.lineWidth <= 1) {
+        lineWidthDirection = !lineWidthDirection;
+    }
+    console.log(lineWidthDirection);
+    console.log(ctx.lineWidth);
+    if (lineWidthDirection) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
 }
 
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
---
+++ Day ten/main.js
@@ -0,0 +1,32 @@
+// let shiftPressed = false;
+// const inbox = document.querySelector('.inbox');
+// const inputCheckBox = [...document.querySelectorAll('input[type="checkbox"]')];
+// console.log(inputCheckBox);
+
+// function findCheckedBox() {
+//     let checkedIndex = inputCheckBox.findIndex(checkbox => checkbox.checked == true);
+//     console.log(checkedIndex);
+// }
+
+// inbox.addEventListener('change', findCheckedBox)
+const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+console.log(checkBoxes);
+
+let lastChecked;
+function handleCheck(e) {
+    // Check if they had the shift key down
+    // And check that they are checking it
+    // The shiftKey property returns a Boolean value that indicates whether or not the "SHIFT" key was pressed when the mouse event was triggered.
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkBoxes.forEach(checkbox => {
+            console.log(checkbox);
+        });
+    }
+
+    lastChecked = this;
+    console.log(lastChecked);
+    console.log(e);
+}
+
+checkBoxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
