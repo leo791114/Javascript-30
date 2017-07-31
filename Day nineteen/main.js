f67bf0c27ef5bd44b0179ea668afe7a47030f382
leo791114 <leo791114@hotmail.com>
2017-07-31 19:37:34 +0800

    day twenty

--- Day nineteen/main.js
+++ Day nineteen/main.js
@@ -28,11 +28,12 @@ function paintToCanvas() {
         // take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
         // mess with them
-        console.log(pixels);
+        // console.log(pixels);
         // pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
-        ctx.globalAlpha = 0.1;
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.1;
         // put them back
+        pixels = greenScreen(pixels);
         ctx.putImageData(pixels, 0, 0);
         // debugger;
     }, 16);
@@ -79,10 +80,35 @@ function rgbSplit(pixels) {
 }
 
 function greenScreen(pixels) {
+    const levels = {};
 
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+        // console.log(levels);
+    });
+
+    for (i = 0; i < pixels.data.length; i += 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax
+        ) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+    return pixels;
 }
 
 getVideo();
+greenScreen();
 
 
 video.addEventListener('canplay', paintToCanvas);
